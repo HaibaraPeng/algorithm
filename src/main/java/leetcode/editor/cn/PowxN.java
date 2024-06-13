d08ce2170d603e158477e9b8f2fa3b4eb1273259
Roc <dongpeng@aeke.com>
2024-06-13 17:02:42 +0800

    feat:PowxN

---
+++ src/main/java/leetcode/editor/cn/PowxN.java
@@ -0,0 +1,68 @@
+//实现 pow(x, n) ，即计算 x 的整数 n 次幂函数（即，xⁿ ）。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：x = 2.00000, n = 10
+//输出：1024.00000
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：x = 2.10000, n = 3
+//输出：9.26100
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：x = 2.00000, n = -2
+//输出：0.25000
+//解释：2-2 = 1/22 = 1/4 = 0.25
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// -100.0 < x < 100.0 
+// -231 <= n <= 231-1 
+// n 是一个整数 
+// 要么 x 不为零，要么 n > 0 。 
+// -104 <= xⁿ <= 104 
+// 
+//
+// Related Topics 递归 数学 👍 1352 👎 0
+
+/**
+ * @author dongp
+ */
+package leetcode.editor.cn;
+
+public class PowxN {
+    public static void main(String[] args) {
+        Solution solution = new PowxN().new Solution();
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public double myPow(double x, int n) {
+            long N = n;
+            return N >= 0 ? quickMul(x, N) : 1.0 / quickMul(x, -N);
+        }
+
+        public double quickMul(double x, long N) {
+            if (N == 0) {
+                return 1.0;
+            }
+            double y = quickMul(x, N / 2);
+            return N % 2 == 0 ? y * y : y * y * x;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
