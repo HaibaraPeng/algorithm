a173176c760edcc5979f3a1ba8e8c1fccdca10fb
Roc <dongpeng@aeke.com>
2024-07-04 18:00:04 +0800

    feat:BinaryTreeZigzagLevelOrderTraversal

---
+++ src/main/java/leetcode/editor/cn/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,128 @@
+//给你二叉树的根节点 root ，返回其节点值的 锯齿形层序遍历 。（即先从左往右，再从右往左进行下一层遍历，以此类推，层与层之间交替进行）。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：root = [3,9,20,null,null,15,7]
+//输出：[[3],[20,9],[15,7]]
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：root = [1]
+//输出：[[1]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：root = []
+//输出：[]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数目在范围 [0, 2000] 内 
+// -100 <= Node.val <= 100 
+// 
+//
+// Related Topics 树 广度优先搜索 二叉树 👍 901 👎 0
+
+/**
+ * @author dongp
+ */
+package leetcode.editor.cn;
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class BinaryTreeZigzagLevelOrderTraversal {
+    public static void main(String[] args) {
+        Solution solution = new BinaryTreeZigzagLevelOrderTraversal().new Solution();
+        TreeNode node = new TreeNode(3);
+        node.left = new TreeNode(9);
+        node.right = new TreeNode(20);
+        node.right.left = new TreeNode(15);
+        node.right.right = new TreeNode(7);
+        solution.zigzagLevelOrder(node);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+            List<List<Integer>> res = new ArrayList<>();
+            if (root == null) {
+                return res;
+            }
+            Deque<TreeNode> queue = new LinkedList<>();
+            queue.addLast(root);
+            boolean flag = true;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                Deque<Integer> list = new LinkedList<>();
+                for (int i = 0; i < size; i++) {
+                    TreeNode treeNode = queue.removeFirst();
+                    if (flag) {
+                        list.addLast(treeNode.val);
+                    } else {
+                        list.addFirst(treeNode.val);
+                    }
+                    if (treeNode.left != null) {
+                        queue.addLast(treeNode.left);
+                    }
+                    if (treeNode.right != null) {
+                        queue.addLast(treeNode.right);
+                    }
+                }
+                flag = !flag;
+                res.add(new ArrayList<>(list));
+            }
+            return res;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
