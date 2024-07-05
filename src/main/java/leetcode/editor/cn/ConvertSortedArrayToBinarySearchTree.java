b7036b85e45b0837db34095da6473d8742786c37
Roc <dongpeng@aeke.com>
2024-07-05 17:23:59 +0800

    feat:ConvertSortedArrayToBinarySearchTree

---
+++ src/main/java/leetcode/editor/cn/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,102 @@
+//给你一个整数数组 nums ，其中元素已经按 升序 排列，请你将其转换为一棵 平衡 二叉搜索树。 
+//
+// 
+//
+// 示例 1： 
+// 
+// 
+//输入：nums = [-10,-3,0,5,9]
+//输出：[0,-3,9,-10,null,5]
+//解释：[0,-10,5,null,-3,null,9] 也将被视为正确答案：
+//
+// 
+//
+// 示例 2： 
+// 
+// 
+//输入：nums = [1,3]
+//输出：[3,1]
+//解释：[1,null,3] 和 [3,1] 都是高度平衡二叉搜索树。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// nums 按 严格递增 顺序排列 
+// 
+//
+// Related Topics 树 二叉搜索树 数组 分治 二叉树 👍 1529 👎 0
+
+/**
+ * @author dongp
+ */
+package leetcode.editor.cn;
+
+public class ConvertSortedArrayToBinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new ConvertSortedArrayToBinarySearchTree().new Solution();
+        solution.sortedArrayToBST(new int[]{-10, -3, 0, 5, 9});
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class Solution {
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return dfs(nums, 0, nums.length - 1);
+        }
+
+        private TreeNode dfs(int[] nums, int left, int right) {
+            if (left < 0 || right > nums.length - 1 || left > right) {
+                return null;
+            }
+            if (left == right) {
+                return new TreeNode(nums[left]);
+            }
+            int mid = left + (right - left) / 2;
+            TreeNode node = new TreeNode(nums[mid]);
+            node.left = dfs(nums, left, mid - 1);
+            node.right = dfs(nums, mid + 1, right);
+            return node;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
