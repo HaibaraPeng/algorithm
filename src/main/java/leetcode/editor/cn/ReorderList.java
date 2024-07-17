a7d61bd72b967d0d71d2b79a23bfe81a60c874a0
Roc <dongpeng@aeke.com>
2024-07-17 15:25:48 +0800

    feat:ReorderList

---
+++ src/main/java/leetcode/editor/cn/ReorderList.java
@@ -0,0 +1,121 @@
+//给定一个单链表 L 的头节点 head ，单链表 L 表示为： 
+//
+// 
+//L0 → L1 → … → Ln - 1 → Ln
+// 
+//
+// 请将其重新排列后变为： 
+//
+// 
+//L0 → Ln → L1 → Ln - 1 → L2 → Ln - 2 → … 
+//
+// 不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：head = [1,2,3,4]
+//输出：[1,4,2,3] 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：head = [1,2,3,4,5]
+//输出：[1,5,2,4,3] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 链表的长度范围为 [1, 5 * 10⁴] 
+// 1 <= node.val <= 1000 
+// 
+//
+// Related Topics 栈 递归 链表 双指针 👍 1485 👎 0
+
+/**
+ * @author dongp
+ */
+package leetcode.editor.cn;
+
+public class ReorderList {
+    public static void main(String[] args) {
+        Solution solution = new ReorderList().new Solution();
+        ListNode node = new ListNode(1);
+        node.next = new ListNode(2);
+        node.next.next = new ListNode(3);
+        node.next.next.next = new ListNode(4);
+        node.next.next.next.next = new ListNode(5);
+        solution.reorderList(node);
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public void reorderList(ListNode head) {
+            ListNode slow = head, fast = head;
+            while (fast.next != null && fast.next.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            ListNode right = slow.next;
+            ListNode reverseRight = reverse(right);
+            slow.next = null;
+            ListNode node = head;
+            while (reverseRight != null) {
+                ListNode next = node.next;
+                ListNode reverseRightNext = reverseRight.next;
+                node.next = reverseRight;
+                reverseRight.next = next;
+                node = next;
+                reverseRight = reverseRightNext;
+            }
+        }
+
+        private ListNode reverse(ListNode node) {
+            if (node == null || node.next == null) {
+                return node;
+            }
+            ListNode last = reverse(node.next);
+            node.next.next = node;
+            node.next = null;
+            return last;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
