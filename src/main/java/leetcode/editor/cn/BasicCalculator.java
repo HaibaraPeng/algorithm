8856a022027f49d18cf8b9c9c9c8ed17bbb01184
Roc <dongpeng@aeke.com>
2024-08-16 11:06:06 +0800

    feat:BasicCalculator

---
+++ src/main/java/leetcode/editor/cn/BasicCalculator.java
@@ -0,0 +1,122 @@
+//给你一个字符串表达式 s ，请你实现一个基本计算器来计算并返回它的值。 
+//
+// 注意:不允许使用任何将字符串作为数学表达式计算的内置函数，比如 eval() 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：s = "1 + 1"
+//输出：2
+// 
+//
+// 示例 2： 
+//
+// 
+//输入：s = " 2-1 + 2 "
+//输出：3
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：s = "(1+(4+5+2)-3)+(6+8)"
+//输出：23
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= s.length <= 3 * 10⁵ 
+// s 由数字、'+'、'-'、'('、')'、和 ' ' 组成 
+// s 表示一个有效的表达式 
+// '+' 不能用作一元运算(例如， "+1" 和 "+(2 + 3)" 无效) 
+// '-' 可以用作一元运算(即 "-1" 和 "-(2 + 3)" 是有效的) 
+// 输入中不存在两个连续的操作符 
+// 每个数字和运行的计算将适合于一个有符号的 32位 整数 
+// 
+//
+// Related Topics 栈 递归 数学 字符串 👍 1059 👎 0
+
+/**
+ * @author dongp
+ */
+package leetcode.editor.cn;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BasicCalculator {
+    public static void main(String[] args) {
+        Solution solution = new BasicCalculator().new Solution();
+        solution.calculate("(1+(4+5+2)-3)+(6+8)");
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int calculate(String s) {
+            // 存放所有的数字
+            Deque<Integer> nums = new ArrayDeque<>();
+            // 为了防止第一个数为负数，先往 nums 加个 0
+            nums.addLast(0);
+            // 将所有的空格去掉
+            s = s.replaceAll(" ", "");
+            // 存放所有的操作，包括 +/-
+            Deque<Character> ops = new ArrayDeque<>();
+            int n = s.length();
+            char[] cs = s.toCharArray();
+            for (int i = 0; i < n; i++) {
+                char c = cs[i];
+                if (c == '(') {
+                    ops.addLast(c);
+                } else if (c == ')') {
+                    // 计算到最近一个左括号为止
+                    while (!ops.isEmpty()) {
+                        char op = ops.peekLast();
+                        if (op != '(') {
+                            calc(nums, ops);
+                        } else {
+                            ops.pollLast();
+                            break;
+                        }
+                    }
+                } else {
+                    if (isNum(c)) {
+                        int u = 0, j = i;
+                        // 将从 i 位置开始后面的连续数字整体取出，加入 nums
+                        while (j < n && isNum(cs[j])) u = u * 10 + (int) (cs[j++] - '0');
+                        nums.addLast(u);
+                        i = j - 1;
+                    } else {
+                        if (i > 0 && (cs[i - 1] == '(' || cs[i - 1] == '+' || cs[i - 1] == '-')) {
+                            nums.addLast(0);
+                        }
+                        // 有一个新操作要入栈时，先把栈内可以算的都算了
+                        while (!ops.isEmpty() && ops.peekLast() != '(') calc(nums, ops);
+                        ops.addLast(c);
+                    }
+                }
+            }
+            while (!ops.isEmpty()) calc(nums, ops);
+            return nums.peekLast();
+        }
+
+        void calc(Deque<Integer> nums, Deque<Character> ops) {
+            if (nums.isEmpty() || nums.size() < 2) return;
+            if (ops.isEmpty()) return;
+            int b = nums.pollLast(), a = nums.pollLast();
+            char op = ops.pollLast();
+            nums.addLast(op == '+' ? a + b : a - b);
+        }
+
+        boolean isNum(char c) {
+            return Character.isDigit(c);
+        }
+
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
