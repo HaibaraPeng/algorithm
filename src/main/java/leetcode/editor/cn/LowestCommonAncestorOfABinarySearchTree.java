007eedb90bd5c22a77b06d4467deae484abd824d
Roc <dongpeng@aeke.com>
2024-08-23 14:37:23 +0800

    feat:LowestCommonAncestorOfABinarySearchTree

---
+++ src/main/java/leetcode/editor/cn/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,98 @@
+//给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。 
+//
+// 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（
+//一个节点也可以是它自己的祖先）。” 
+//
+// 例如，给定如下二叉搜索树: root = [6,2,8,0,4,7,9,null,null,3,5] 
+//
+// 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+//输出: 6 
+//解释: 节点 2 和节点 8 的最近公共祖先是 6。
+// 
+//
+// 示例 2: 
+//
+// 输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+//输出: 2
+//解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。 
+//
+// 
+//
+// 说明: 
+//
+// 
+// 所有节点的值都是唯一的。 
+// p、q 为不同节点且均存在于给定的二叉搜索树中。 
+// 
+//
+// Related Topics 树 深度优先搜索 二叉搜索树 二叉树 👍 1269 👎 0
+
+/**
+ * @author dongp
+ */
+package leetcode.editor.cn;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+    public static void main(String[] args) {
+        Solution solution = new LowestCommonAncestorOfABinarySearchTree().new Solution();
+        TreeNode node = new TreeNode(6);
+        node.left = new TreeNode(2);
+        node.right = new TreeNode(8);
+        node.left.left = new TreeNode(0);
+        node.left.right = new TreeNode(4);
+        node.right.left = new TreeNode(7);
+        node.right.right = new TreeNode(9);
+        node.left.right.left = new TreeNode(3);
+        node.left.right.right = new TreeNode(5);
+        solution.lowestCommonAncestor(node, node.left, node.left.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    //leetcode submit region begin(Prohibit modification and deletion)
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode(int x) { val = x; }
+     * }
+     */
+
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null || root == p || root == q) {
+                return root;
+            }
+            if (p.val < root.val && q.val > root.val) {
+                return root;
+            }
+            if (q.val < root.val && p.val > root.val) {
+                return root;
+            }
+            if (p.val < root.val && q.val < root.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            } else {
+                return lowestCommonAncestor(root.right, p, q);
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
